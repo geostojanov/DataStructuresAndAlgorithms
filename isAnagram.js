bd1cb4c0d4fd3c31f251edd421635aa16a536fe3
George Stojanov <gestojanov@gmail.com>
2020-08-20 19:02:16 +0200

    Add isAnagram function with frequency  counter solution

---
+++ isAnagram.js
@@ -0,0 +1,20 @@
+function isAnagram(string1, string2) {
+    if (string1.length != string2.length) {
+      return false;
+    }
+    let lookup = {};
+    for(let item of string1) {
+      lookup[item] = (lookup[item] || 0) + 1;
+    }
+
+    for(let char of string2) {
+      if (!(char in lookup)) {
+        return false;
+      } else {
+        lookup[char] -= 1;
+      }
+    }
+    return true;
+}
+
+console.log(isAnagram('iceman', 'cinema'));
