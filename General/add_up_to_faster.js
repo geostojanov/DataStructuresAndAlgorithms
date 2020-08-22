97c3d09b94a99bd65bc144700e9215d41cdd66ae
George Stojanov <gestojanov@gmail.com>
2020-08-22 16:12:48 +0200

    Refactor directory structure

---
+++ General/add_up_to_faster.js
@@ -0,0 +1,10 @@
+function addUpTo(n) {
+    return n * (n + 1) / 2;
+}
+
+let t1 = Date.now();
+let result = addUpTo(10000000000);
+let t2 = Date.now();
+
+console.log(result);
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds.`);
